8f3d0a37e5f24c5846e84c0fc717061ce75b9015
stasbar <stasbar1995@gmail.com>
2020-06-02 01:20:31 +0200

    write csv output

--- src/simulator/simulator.ts
+++ src/simulator/simulator.ts
@@ -18,34 +18,54 @@ if (argv.in) {
 }
 const env = new Environment(data);
 
-const publications: number = Number(argv.publications || 30);
-const xi: number = Number(argv.xi || 0.25);
-const tau: number = Number(argv.tau || 200);
-const zhq: number = Number(argv.zhq || 1);
-const zia: number = Number(argv.zhq || 100);
+function performTest() {
+  const publications: number = Number(argv.publications || 30);
+  const xi: number = Number(argv.xi || 0.25);
+  const tau: number = Number(argv.tau || 200);
+  const zhq: number = Number(argv.zhq || 1);
+  const zia: number = Number(argv.zia || 100);
 
-let extinctions = 0;
-let epidemics = 0;
-const iterations: number = Number(argv.iterations || 50);
-console.dir({
-  nodes: env.data.nodes.length,
-  edges: env.data.edges.size,
-  publications,
-  xi,
-  tau,
-  zhq,
-  zia,
-  iterations
-});
-for (let index = 0; index < iterations; index++) {
-  env.reset();
-  env.start(publications, xi, tau, zhq, zia);
+  let extinctions = 0;
+  let epidemics = 0;
+  const iterations: number = Number(argv.iterations || 50);
+  console.dir({
+    nodes: env.data.nodes.length,
+    edges: env.data.edges.size,
+    publications,
+    xi,
+    tau,
+    zhq,
+    zia,
+    iterations
+  });
+  for (let index = 0; index < iterations; index++) {
+    env.reset();
+    env.start(publications, xi, tau, zhq, zia);
 
-  if (env.extinction) extinctions++;
-  else if (env.epidemic) epidemics++;
+    if (env.extinction) extinctions++;
+    else if (env.epidemic) epidemics++;
+  }
+  console.log(
+    `\nExtinctions/Epidemics ${extinctions}/${epidemics} = ${(extinctions /
+      iterations) *
+      100}/${(epidemics / iterations) * 100}`
+  );
+  return Math.round((extinctions / iterations) * 100);
 }
-console.log(
-  `Extinctions/Epidemics ${extinctions}/${epidemics} = ${(extinctions /
-    iterations) *
-    100}/${(epidemics / iterations) * 100}`
-);
+
+if (!argv.all) {
+  performTest();
+} else {
+  const results = [25, 50, 100, 200, 300, 500].map(zia => {
+    argv.zia = zia;
+    const extinctions = performTest();
+    return { zia, extinctions };
+  });
+
+  const writer = fs.createWriteStream("outputs.csv");
+  results.forEach(({ zia, extinctions }) => {
+    writer.write(`"${zia}","${extinctions}"\n`);
+  });
+
+  writer.close();
+}
